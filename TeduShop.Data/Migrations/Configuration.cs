6dc662de8f521460349b8ed836da674d3474db1a
NGUYENHIEU191\admin <nguyenhoanghieu191@gmail.com>
2018-11-19 08:41:01 +0700

    commit ng-repeat - productcategory

--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -1,9 +1,10 @@
-namespace TeduShop.Data.Migrations
+﻿namespace TeduShop.Data.Migrations
 {
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Model.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -17,31 +18,49 @@ namespace TeduShop.Data.Migrations
 
         protected override void Seed(TeduShop.Data.TeduShopDbContext context)
         {
-            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TeduShopDbContext()));
+            AddInfomation_ProductCategory(context);
 
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TeduShopDbContext()));
+            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TeduShopDbContext()));
 
-            var user = new ApplicationUser()
+            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TeduShopDbContext()));
+
+            //var user = new ApplicationUser()
+            //{
+            //    UserName = "tedu",
+            //    Email = "tedu.international@gmail.com",
+            //    EmailConfirmed = true,
+            //    Brithday = DateTime.Now,
+            //    FullName = "Technology Education"
+            //};
+
+            //manager.Create(user, "123654$");
+
+            //if (!roleManager.Roles.Any())
+            //{
+            //    roleManager.Create(new IdentityRole { Name = "Admin" });
+            //    roleManager.Create(new IdentityRole { Name = "User" });
+            //}
+
+            //var adminUser = manager.FindByEmail("tedu.international@gmail.com");
+
+            //manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+
+        }
+
+        private void AddInfomation_ProductCategory(TeduShop.Data.TeduShopDbContext context)
+        {
+            if (context.ProductCategories.Count() == 0)
             {
-                UserName = "tedu",
-                Email = "tedu.international@gmail.com",
-                EmailConfirmed = true,
-                Brithday = DateTime.Now,
-                FullName = "Technology Education"
-            };
-
-            manager.Create(user, "123654$");
-
-            if (!roleManager.Roles.Any())
-            {
-                roleManager.Create(new IdentityRole { Name = "Admin" });
-                roleManager.Create(new IdentityRole { Name = "User" });
+                List<ProductCategory> productCategory = new List<ProductCategory>()
+                {
+                    new ProductCategory() { Name="Điện lanh", Alias="dien-lanh", Status=true },
+                    new ProductCategory() { Name="viễn thông", Alias="vien-thong", Status=true },
+                    new ProductCategory() { Name="Đồ gia dụng", Alias="do-gia-dung", Status=true },
+                    new ProductCategory() { Name="Mỹ phẩm", Alias="my-pham", Status=true }
+                };
+                context.ProductCategories.AddRange(productCategory);
+                context.SaveChanges();
             }
-
-            var adminUser = manager.FindByEmail("tedu.international@gmail.com");
-
-            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
-
         }
     }
 }
